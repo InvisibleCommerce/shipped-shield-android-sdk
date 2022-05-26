abeb1478a348642ed1c5fdb4a79c7b07632cd518
Victor Zhu <ghoyelo@gmail.com>
2022-05-26 16:04:03 +0800

    Add tests

--- shippedshield/src/test/java/com/invisiblecommerce/shippedshield/model/ShieldRequestTest.kt
+++ shippedshield/src/test/java/com/invisiblecommerce/shippedshield/model/ShieldRequestTest.kt
@@ -10,11 +10,18 @@ class ShieldRequestTest {
     private val defaultOrderValue = BigDecimal.valueOf(129.99)
 
     private val request = ShieldRequest.Builder().setOrderValue(defaultOrderValue).build()
+    private val emptyRequest = ShieldRequest.Builder().build()
 
     @Test
     fun testParams() {
         assertNotNull(request.toParamMap())
         assertEquals(ShieldRequest.Builder().setOrderValue(defaultOrderValue).build().toParamMap(), request.toParamMap())
+    }
+
+    @Test
+    fun testEmptyParams() {
+        assertNotNull(emptyRequest.toParamMap())
+        assertEquals(ShieldRequest.Builder().build().toParamMap(), emptyRequest.toParamMap())
     }
 
     @Test
--- shippedshield/src/test/java/com/invisiblecommerce/shippedshield/model/parser/ShieldOfferParserTest.kt
+++ shippedshield/src/test/java/com/invisiblecommerce/shippedshield/model/parser/ShieldOfferParserTest.kt
@@ -1,15 +1,53 @@
 package com.invisiblecommerce.shippedshield.model.parser
 
+import android.os.Build.VERSION
 import org.json.JSONObject
 import org.junit.Test
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
 import java.math.BigDecimal
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
+
 class ShieldOfferParserTest {
+
+    @Throws(Exception::class)
+    fun setFinalStatic(field: Field, newValue: Any?) {
+        field.setAccessible(true)
+        val modifiersField: Field = Field::class.java.getDeclaredField("modifiers")
+        modifiersField.setAccessible(true)
+        modifiersField.setInt(field, field.getModifiers() and Modifier.FINAL.inv())
+        field.set(null, newValue)
+    }
 
     @Test
     fun parserTest() {
+        val jsonObject = JSONObject(
+            """
+                {
+                "storefront_id": "test-paws.myshopify.com",
+                "order_value": "129.99",
+                "shield_fee": "2.27",
+                "offered_at": "2022-05-18T18:03:22.252-07:00"
+                }
+            """.trimIndent()
+        )
+
+        val parser = ShieldOfferParser()
+        assertNotNull(parser.dateFormat)
+
+        val offer = parser.parse(jsonObject)
+        assertEquals(offer.storefrontId, "test-paws.myshopify.com")
+        assertEquals(offer.orderValue, BigDecimal.valueOf(129.99))
+        assertEquals(offer.shieldFee, BigDecimal.valueOf(2.27))
+        assertEquals(offer.offeredAt, parser.dateFormat.parse("2022-05-18T18:03:22.252-07:00"))
+    }
+
+    @Test
+    fun switchBuldVersion() {
+        setFinalStatic(VERSION::class.java.getField("SDK_INT"), 25)
+
         val jsonObject = JSONObject(
             """
                 {
