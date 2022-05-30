aa21426cf3060292a8ad95dd44d15805df3e39df
Victor Zhu <ghoyelo@gmail.com>
2022-05-30 11:13:52 +0800

    Learn more dialog test

---
+++ shippedshield/src/test/java/com/invisiblecommerce/shippedshield/widget/LearnMoreTest.kt
@@ -0,0 +1,26 @@
+package com.invisiblecommerce.shippedshield.widget
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.invisiblecommerce.shippedshield.ShippedShield
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import java.math.BigDecimal
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [29])
+class LearnMoreTest {
+
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
+    private val learnMoreDialog: LearnMoreDialog by lazy {
+        LearnMoreDialog(context)
+    }
+
+    @Test
+    fun dialogTest() {
+        learnMoreDialog
+    }
+}
